16f128f6c762899b6d19da3adbade0a5a689c1aa
realknack <97179804+realknack@users.noreply.github.com>
2023-07-13 02:16:27 -0400

    Create firstpython.py
    car function start modified

---
+++ firstpython.py
@@ -0,0 +1,27 @@
+command = ""
+started = True
+while True:
+    command = input('> ').lower()
+    if command == "start":
+        if started:
+            print("Car already started")
+        else:
+            started = True
+            print("Car Started...")
+
+    elif command == "stop":
+        if not started:
+            print("Car is already stopped...")
+        else:
+            started = False
+            print("Car Stopped")
+    elif command == "help":
+        print("""
+start -- to start the car
+stop -- to stop the car
+quite -- to quite
+    """)
+    elif command == "quit":
+        break
+    else:
+        print("i dont understand")
